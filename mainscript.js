6b57ff9998260e5248a9d088a423822dcc017a3b
SvenLuttik <61277332+SvenLuttik@users.noreply.github.com>
2020-02-27 23:22:51 +0100

    Update - nieuwe charts
    Kijken of deze charts het wel goed doen

--- mainscript.js
+++ mainscript.js
@@ -1,63 +1,69 @@
-/**
- * ---------------------------------------
- * This demo was created using amCharts 4.
- * 
- * For more information visit:
- * https://www.amcharts.com/
- * 
- * Documentation is available at:
- * https://www.amcharts.com/docs/v4/
- * ---------------------------------------
- */
+window.onload = function () {
 
-// Themes begin
-am4core.useTheme(am4themes_dataviz);
-am4core.useTheme(am4themes_animated);
-// Themes end
+/*          zwaartekracht          */
+    
+var chart = new CanvasJS.Chart("gravity", {
+	exportEnabled: true,
+	animationEnabled: true,
+	axisX: {
+		title: "Zwaartekracht in Newton"
+	},
+	
+	axisY2: {
+		title: "Units",
+		titleFontColor: "#000",
+		lineColor: "#C0504E",
+		labelFontColor: "#C0504E",
+		tickColor: "#fff"
+	},
+	toolTip: {
+		shared: true
+	},
+	legend: {
+		cursor: "pointer",
+		itemclick: toggleDataSeries
+	},
+	data: [{
+		type: "column",
+		name: "Zwartekracht",
+		showInLegend: true,      
+		yValueFormatString: "#,##0.# Newton",
+		dataPoints: [
+			{ label: "Aarde",  y: 9.807 },
+			{ label: "Shuttle", y: 0.20 },
+			{ label: "Mars", y: 3.711 },
+		]
+	},
+]
+});
+chart.render();
 
-// create chart
-var chart = am4core.create("chartdiv", am4charts.GaugeChart);
-chart.hiddenState.properties.opacity = 0; // this makes initial fade in effect
-
-chart.innerRadius = -25;
-
-var axis = chart.xAxes.push(new am4charts.ValueAxis());
-axis.min = 0;
-axis.max = 100;
-axis.strictMinMax = true;
-axis.renderer.grid.template.stroke = new am4core.InterfaceColorSet().getFor("background");
-axis.renderer.grid.template.strokeOpacity = 0.3;
-
-var colorSet = new am4core.ColorSet();
-
-var range0 = axis.axisRanges.create();
-range0.value = 0;
-range0.endValue = 50;
-range0.axisFill.fillOpacity = 1;
-range0.axisFill.fill = colorSet.getIndex(0);
-range0.axisFill.zIndex = - 1;
-
-var range1 = axis.axisRanges.create();
-range1.value = 50;
-range1.endValue = 80;
-range1.axisFill.fillOpacity = 1;
-range1.axisFill.fill = colorSet.getIndex(2);
-range1.axisFill.zIndex = -1;
-
-var range2 = axis.axisRanges.create();
-range2.value = 80;
-range2.endValue = 100;
-range2.axisFill.fillOpacity = 1;
-range2.axisFill.fill = colorSet.getIndex(4);
-range2.axisFill.zIndex = -1;
-
-var hand = chart.hands.push(new am4charts.ClockHand());
-
-// using chart.setTimeout method as the timeout will be disposed together with a chart
-chart.setTimeout(randomValue, 2000);
-
-function randomValue() {
-    hand.showValue(Math.random() * 100, 1000, am4core.ease.cubicOut);
-    chart.setTimeout(randomValue, 2000);
+function toggleDataSeries(e) {
+	if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	} else {
+		e.dataSeries.visible = true;
+	}
+	e.chart.render();
 }
 
+/*          Snelheid          */
+
+var chart = new CanvasJS.Chart("speed", {
+	animationEnabled: true,
+	title:{
+		horizontalAlign: "left"
+	},
+	data: [{
+		type: "doughnut",
+		startAngle: 180,
+		//innerRadius: 60,
+		dataPoints: [
+			{ y: 5, label: "current speed"},
+			{ y: 6, label: "top speed"}
+		]
+	}]
+});
+chart.render();
+
+}
